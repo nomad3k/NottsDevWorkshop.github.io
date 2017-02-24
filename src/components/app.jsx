17caffede0060cf40af2fcb60a76b46749fac6fa
Chris Kemp <chris.kemp3000@gmail.com>
2017-02-24 22:16:58 +0000

    Added link to logo

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,13 +2,16 @@ import 'normalize.css/normalize.css';
 import 'styles/app.css';
 
 import React from 'react';
+import { Link } from 'react-router';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="app">
         <div className='logo'>
-          <img src='/images/notts_dev_workshop_logo.png' className='logo__img' />
+          <Link to='/'>
+            <img src='/images/notts_dev_workshop_logo.png' className='logo__img' />
+          </Link>
         </div>
         {this.props.children}
       </div>
